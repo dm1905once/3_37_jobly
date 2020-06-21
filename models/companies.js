fc8506bf63f4027cce3fdb079fd8cb2fe1be5267
danielm <dm1905once@gmail.com>
2020-06-20 19:06:07 -0700

    Added companies routes

--- models/companies.js
+++ models/companies.js
@@ -1,6 +1,82 @@
 const db = require("../db");
+const partialUpdate = require("../helpers/partialUpdate")
 
 class Companies{
+
+    static async getCompanies(parameters){
+        const conditions = [];
+        const values = [];
+        let index=1;
+
+        if (parameters.min_employees) { conditions.push(`num_employees >= $${index++}`); values.push(parameters.min_employees); }
+        if (parameters.max_employees) { conditions.push(`num_employees <= $${index++}`); values.push(parameters.max_employees); }
+        if (parameters.search) { conditions.push(`name LIKE $${index++}`); values.push(`%${parameters.search}%`); }
+
+        const selectQuery = "SELECT handle, name FROM companies " + (conditions.length ? ("WHERE " + conditions.join(" AND ")) : "");
+        const companies = await db.query(selectQuery,values);
+
+        return companies.rows;
+    }
+
+    static async getCompany(handle){
+        const company = await db.query(
+            `SELECT handle, name, num_employees, description, logo_url
+            FROM companies
+            WHERE handle = $1`,
+            [handle]
+        );
+        return company.rows[0];
+    }
+
+    static async createCompany(companyObject){
+        const newCompany = await db.query(
+            `INSERT INTO Companies(
+                handle,
+                name,
+                num_employees,
+                description,
+                logo_url)
+            VALUES($1, $2, $3, $4, $5)
+            RETURNING
+                handle,
+                name,
+                num_employees,
+                description,
+                logo_url`,
+            [
+                companyObject.handle,
+                companyObject.name,
+                companyObject.num_employees,
+                companyObject.description,
+                companyObject.logo_url
+            ]);
+            return newCompany.rows[0];
+    }
+
+    static async updateCompany(companyObject, handle){
+        let updateObject = {}; 
+        if (companyObject.handle) updateObject['handle'] = companyObject.handle; 
+        if (companyObject.name) updateObject['name'] = companyObject.name; 
+        if (companyObject.num_employees) updateObject['num_employees'] = companyObject.num_employees; 
+        if (companyObject.description) updateObject['description'] = companyObject.description; 
+        if (companyObject.logo_url) updateObject['logo_url'] = companyObject.logo_url; 
+        const updateQuery = partialUpdate("companies", updateObject, "handle", handle);
+        const updatedCompany = await db.query(updateQuery.query, updateQuery.values);
+        return updatedCompany.rows[0];
+    }
+
+    static async deleteCompany(handle){
+        const company = await db.query(`DELETE FROM companies 
+        WHERE handle = $1
+        RETURNING handle`,
+        [handle]);
+
+        if (company.rows.length === 0) {
+            throw { message: `Company ${handle} does not exist`, status: 404 }
+          } else {
+              return company.rows[0];
+          }
+    }
 
 }
 
--- routes/companies.js
+++ routes/companies.js
@@ -1,18 +1,66 @@
 const express = require("express");
+const jsonschema = require("jsonschema");
+const ExpressError = require("../helpers/expressError");
+const newCompanySchema = require("../schemas/newCompanySchema.json")
+const Companies = require("../models/companies")
+
 const router = new express.Router();
 
-router.get("/", function(req, res, next){
+router.get("/", async function(req, res, next){
     const parameters = req.query;
-    console.log(Object.keys(parameters));
-    console.log(parameters['algo']);
+    if (Object.keys(parameters).includes("min_employees") && Object.keys(parameters).includes("max_employees")){
+        if (parseInt(parameters['min_employees']) > parseInt(parameters['max_employees'])){
+            const error = new ExpressError("min_employees cannot be greater than max_employees", 400);
+            return next(error);
+        }
+    }
+    const allCompanies = await Companies.getCompanies(parameters);
+    res.json({companies: allCompanies})
 })
 
 
-router.get("/:handle", function(req, res, next){
-    console.log(req.params.handle);
-})
+router.post("/", async function(req, res, next){
+    try{
+        const validBook = jsonschema.validate(req.body, newCompanySchema);
+        if (validBook){
+            const company = await Companies.createCompany(req.body);
+            return res.status(201).json({company: company});
+        } else {
+            const errors = validation.errors.map(error => error.stack);
+            const error = new ExpressError(errors, 400);
+            return next(error);
+        }
+    } catch (err) {
+    return next(err);
+  }
+});
 
+router.get("/:handle", async function(req, res, next){
+    try{
+        const company = await Companies.getCompany(req.params.handle);
+        return res.json({company});
+    } catch (err) {
+    return next(err);
+  }
+});
 
+router.patch("/:handle", async function(req, res, next){
+    try{
+        const company = await Companies.updateCompany(req.body, req.params.handle);
+        return res.json({company});
+    } catch (err) {
+    return next(err);
+  }
+});
+
+router.delete("/:handle", async function(req, res, next){
+    try{
+        const company = await Companies.deleteCompany(req.params.handle);
+        if (company) return res.json({"message": "Company deleted"});
+    } catch (err) {
+    return next(err);
+  }
+});
 
 
 module.exports = router;
